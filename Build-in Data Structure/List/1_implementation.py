d2e6dff24ec5ff6ddc3d7f195b2fdc43dae308f6
Sonu Kumar <sonu3660@gmail.com>
2024-08-07 13:40:35 +0530

    Completed list implementation

---
+++ Build-in Data Structure/List/1_implementation.py
@@ -0,0 +1,15 @@
+# Single dimension list
+list = [1, 2, "Hey", 4]
+print(list)         # [1, 2, 'Hey', 4]
+
+# Multi-dimension list
+list_2 = [["Hey", "Sonu"],[1, 2], ["Good Morning"]]
+print(list_2)  # [['Hey', 'Sonu'], [1, 2], ['Good Morning']]
+
+# Get size of any list
+print(len(list))    # 4
+
+# Concept of negative indexing
+list_3 = [1, 2, 3, 4, 5]
+print(list_3[-1])   # 5
+print(list_3[-3])   # 3
