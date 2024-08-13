f34d98433c42e9b074e1c1d2d858fb9c070298be
Sonu Kumar <sonu3660@gmail.com>
2024-08-13 13:18:27 +0530

    Completed basic dictionary syntax

--- Basic Syntax/5_functions.py
+++ Basic Syntax/5_functions.py
@@ -1,33 +1,57 @@
-
 def function_one(test):
     print(test)
-    
-function_one("Hello!!!")    # Hello!!!
+
+
+function_one("Hello!!!")  # Hello!!!
+
 
 def function_two(*allArgs):
-    print(allArgs)      # (1, 2, 3, 4)
-    print(allArgs[0])   # 1
-    print(allArgs[3])   # 4
-    
-function_two(1,2,3,4)   
+    print(allArgs)  # (1, 2, 3, 4)
+    print(allArgs[0])  # 1
+    print(allArgs[3])  # 4
+
+
+function_two(1, 2, 3, 4)
+
 
 def function_three(child3, child2, child1):
     print(child1)
     print(child3)
-    
-function_three(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+function_three(child1="Emil", child2="Tobias", child3="Linus")
+
 
 def pass_function():
     pass
+
+
 pass_function()
 
 
-def receive_only_positional_arguments(arg1,/):
+def receive_only_positional_arguments(arg1, /):
     print(arg1)
-    
+
+
 receive_only_positional_arguments(1)
 
-def receive_only_keyword_arguments(*, arg1,):
+
+def receive_only_keyword_arguments(*, arg1):
     print(arg1)
-    
+
+
 receive_only_keyword_arguments(arg1=2)
+
+
+def return_none():
+    pass
+
+
+print(return_none)  # <function return_none at 0x000001FC4FB894E0>
+
+
+def function_annotation(value: int = 2) -> bool:
+    return True if value == 1 else False
+
+
+print(function_annotation("1"))  # False
---
+++ Build-in Data Structure/Dictonary/2_example.py
@@ -0,0 +1,8 @@
+vehicles = {
+    "dream": "Honda 250T",
+    "roadster": "BMW R1100",
+    "er5": "Kawasaki er5",
+}
+
+print(vehicles["dream"])  # Honda 250T
+print(vehicles.get("er5"))  # Kawasaki er5
